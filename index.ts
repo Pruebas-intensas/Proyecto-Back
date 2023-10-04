417376363d0cbbbd76f2823bbf6bed01569e53c8
Joacodef <joacodef1@gmail.com>
2023-10-04 16:29:46 -0300

    Funciona conexion remota a backend

--- index.ts
+++ index.ts
@@ -1,15 +1,22 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
+const cors = require('cors');
+
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+
+const corsOptions = {
+  origin: ["*"],
+};
+
+app.use(cors(corsOptions));
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+app.listen(parseInt(port || ""), "0.0.0.0", () => { console.log("Running on port " + port || "")});
